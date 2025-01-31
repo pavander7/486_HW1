b3c397b7c922667695b1ce998ee973c46e6f98d2
Paul Vander Woude <pavander@umich.edu>
2025-01-31 11:33:22 -0500

    initial commit

---
+++ pavander.Assignment1/languageIdentification.py
@@ -0,0 +1,13 @@
+# Paul Vander Woude (pavander) EECS 486 HW1 languageIdentification.py
+
+def trainBigramLanguageModel(train_text):
+    pass
+
+def identifyLanguage(test_text, language_names, unigram_char_freq_dicts, bigram_char_freq_dicts):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ pavander.Assignment1/preprocess.py
@@ -0,0 +1,13 @@
+# Paul Vander Woude (pavander) EECS 486 HW1 preprocess.py
+
+def removeSGML(input):
+    pass
+
+def tokenizeText(input):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
